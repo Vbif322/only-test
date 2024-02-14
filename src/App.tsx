dc297fe3809e513216ef016d787f8c50c88a1eba
Vbif322 <hxdfue@yandex.ru>
2024-02-14 15:12:53 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App__container"></div>
+    </div>
+  );
+}
+
+export default App;
